fbcbe0bfcc0783867cacbe5f2a6a8dc021ef0d2d
RyanDBarnett <conner2468@gmail.com>
2020-08-02 12:14:16 -0600

    Add message test to make sure output file name can be different

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -14,4 +14,9 @@ class TranslatorTest < Minitest::Test
   def test_message
     assert_equal "Created 'braille.txt' containing 256 characters", @translator.message
   end
+
+  def test_message_output_file_can_be_different
+    @translator = Translator.new('message.txt', 'different_name.txt')
+    assert_equal "Created 'different_name.txt' containing 256 characters", @translator.message
+  end
 end
