8d35268e348f490e62621b3a2c736c86f6ab169d
RyanDBarnett <conner2468@gmail.com>
2020-08-02 12:38:34 -0600

    Update message so char count is dynamic

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,9 +2,11 @@ class Translator
   def initialize(input_file, output_file)
     @input = input_file
     @output = output_file
+    @input_path = "./data/#{input_file}"
+    @output_path = "./data/#{output_file}"
   end
 
   def message
-    "Created '#{@output}' containing 256 characters"
+    "Created '#{@output}' containing #{File.open(@input_path).size} characters"
   end
 end
